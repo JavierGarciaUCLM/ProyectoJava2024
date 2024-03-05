0e69b8670473f52a444006bcb4d8e0afeb2299fb
junpi42 <junpi42@gmail.com>
2024-03-05 15:49:59 +0100

    Añadir clase medicina

---
+++ src/resources/Medicine.java
@@ -0,0 +1,59 @@
+package resources;
+
+public class Medicine {
+
+    int id;
+    String indications;
+    String manufacturer;
+    int units;
+
+    public Medicine(){}
+
+    /**
+     * Constructor of Medicine
+     * @param id
+     * @param indications
+     * @param manufacturer
+     * @param units
+     */
+    public Medicine(int id, String indications, String manufacturer, int units) {
+        this.id = id;
+        this.indications = indications;
+        this.manufacturer = manufacturer;
+        this.units = units;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIndications() {
+        return indications;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIndications(String indications) {
+        this.indications = indications;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+}
